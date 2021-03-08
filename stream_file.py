4f6e1895158f77feba6c5260b07fa0c29cf730aa
Anoop-A-Anto <75159380+Anoop-A-Anto@users.noreply.github.com>
2021-03-08 19:41:08 +0530

    Update stream_file.py

--- stream_file.py
+++ stream_file.py
@@ -112,6 +112,7 @@ elif choice=="Login":
 
         
         #user_id = st.number_input('user_id',  min_value=1, max_value=53424, value=1)
+        user_id="1"
         user_id=st.text_input("Enter user_id {1-53424} default 1")
         user_id=int(user_id)
         if 1<=user_id<=53424:
@@ -135,6 +136,7 @@ elif choice=="Login":
         recc_df=recc_df.iloc[6:36].reset_index(drop=True)
 
         #num= st.number_input('required_reccomondation_count',  min_value=2, max_value=30, value=5)
+        num="2"
         num=st.text_input("Enter required_reccomondation_count (2-30) default 2")
         num=int(num)
         if 2<=num<=30:
