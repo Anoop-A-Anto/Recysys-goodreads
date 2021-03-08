431eca04f9131a3bd5f1ec39023fd9430cb47bf1
Anoop-A-Anto <75159380+Anoop-A-Anto@users.noreply.github.com>
2021-03-08 19:51:29 +0530

    Update stream_file.py

--- stream_file.py
+++ stream_file.py
@@ -112,11 +112,13 @@ elif choice=="Login":
 
         
         #user_id = st.number_input('user_id',  min_value=1, max_value=53424, value=1)
-        user_id="1"
+        
         user_id=st.text_input("Enter user_id {1-53424} default 1")
-        user_id=int(user_id)
-        if 1<=user_id<=53424:
-            user_id=user_id
+        if user_id!=None:
+            user_id=int(user_id)
+            if user_id<1 or user_id>53424:
+                user_id=1                
+                
         else:
             user_id=1
 
@@ -136,14 +138,17 @@ elif choice=="Login":
         recc_df=recc_df.iloc[6:36].reset_index(drop=True)
 
         #num= st.number_input('required_reccomondation_count',  min_value=2, max_value=30, value=5)
-        num="2"
+        
         num=st.text_input("Enter required_reccomondation_count (2-30) default 2")
-        num=int(num)
-        if 2<=num<=30:
-            num=num
+        
+                    
+        if num!=None:
+            num=int(num)
+            if num<2 or num>30:
+                num=2                
+                
         else:
             num=2
-
         
         recc_df_table =recc_df.iloc[:num]
         recc_df_table=pd.merge(recc_df,titlefile,left_on="book_id",right_on="book_id")
