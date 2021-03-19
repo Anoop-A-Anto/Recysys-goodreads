9252ccc035bcf840a51d2f4053240e7905a9e499
Anoop-A-Anto <75159380+Anoop-A-Anto@users.noreply.github.com>
2021-03-19 14:11:01 +0530

    Update stream_file.py

--- stream_file.py
+++ stream_file.py
@@ -253,16 +253,14 @@ main_bg_ext = "jpg"
 side_bg = "janko-ferlic-sfL_QOnmy00-unsplash.jpg"
 side_bg_ext = "jpg"
 
-st.markdown(
-    f"""
+image_string=f"""
     <style>
     .reportview-container {{
-        background: url(data:image/{main_bg_ext};base64,{base64.b64encode(open(main_bg, "rb").read()).decode()})
+        background: url(data:image/{main_bg_ext};base64,{base64.b64encode(open(main_bg, "rb").read()).decode()};background-size: cover)
     }}
-   .sidebar .sidebar-content {{
+   .sidebar.sidebar-content {{
         background: url(data:image/{side_bg_ext};base64,{base64.b64encode(open(side_bg, "rb").read()).decode()})
     }}
     </style>
-    """,
-    unsafe_allow_html=True
-)
+    """
+st.markdown(image_string,unsafe_allow_html=True)
