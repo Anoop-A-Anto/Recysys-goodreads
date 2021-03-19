647f5e4e715fc757038141bd68f8bf15b223d735
Anoop-A-Anto <75159380+Anoop-A-Anto@users.noreply.github.com>
2021-03-19 15:02:59 +0530

    Update stream_file.py

--- stream_file.py
+++ stream_file.py
@@ -247,8 +247,8 @@ elif choice=="Books":
     
     
 #bg image setting
-#main_bg = "jez-timms-8muUTAmcWU4-unsplash.jpg"
-main_bg ="hieu-vu-minh-He8-FZl-o10-unsplash.jpg"
+
+main_bg ="henry-be--Pg63JThyCg-unsplash.jpg"
 main_bg_ext = "jpg"
 
 side_bg = "dbe7f843-39a6-42df-a52c-e2e94b5bc1cd.jpg"
